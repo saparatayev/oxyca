910bbcba0a469bebf15b8cb8e4c7017dc924a4a9
saparatayev <sopa.97@mail.ru>
2021-10-22 14:05:28 +0500

    show culumn value from a pivot table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    public function products()
+    {
+        return $this->belongsToMany('App\Models\Product','orders_products')->withPivot('quantity');
+    }
 }
---
+++ resources/views/orders/show.blade.php
@@ -0,0 +1,5 @@
+                        <td class="cart_quantity">
+                            <div class="cart_quantity_button">
+                                <input class="cart_quantity_input" type="text" name="quantity" value="{{ $prod->pivot->quantity }}" autocomplete="off" size="2" disabled>
+                            </div>
+                        </td>
