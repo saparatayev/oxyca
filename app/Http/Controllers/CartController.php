73941f671051a9e2c091fb5d07c9ccd97c22a7ec
saparatayev <sopa.97@mail.ru>
2021-10-28 12:25:04 +0500

    notification by email

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use App\Notifications\OrderSaved;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -242,6 +243,8 @@ class CartController extends AdminController
             foreach($products as $p) {
                 $order->products()->attach($p->id, ['quantity' => $productsInCart[$p->id]]);
             }
+
+            auth()->user()->notify(new OrderSaved($order));
 
             $this->clear($request);
 
---
+++ app/Notifications/OrderSaved.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderSaved extends Notification
+{
+    use Queueable;
+
+    /**
+     * The order which is saved by one of admins
+     * @var Order
+     */
+    public $order;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('A new order was saved.')
+                    ->action('View the order', route('orders.show', ['order' => $this->order]))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
